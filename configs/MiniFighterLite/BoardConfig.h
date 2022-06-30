baa4a5a88eda0f388a4ea5e9232f84337e7ffa03
CocoaFudge <42714371+CocoaFudge@users.noreply.github.com>
2022-06-30 20:46:18 +0100

    Add files via upload

---
+++ configs/MiniFighterLite/BoardConfig.h
@@ -0,0 +1,34 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * SPDX-FileCopyrightText: Copyright (c) 2021 Jason Skuby (mytechtoybox.com)
+ */
+
+#ifndef MINI_FIGHTER_LITE_BOARD_CONFIG_H_
+#define MINI_FIGHTER_LITE_BOARD_CONFIG_H_
+
+#include <GamepadEnums.h>
+
+#define PIN_DPAD_UP     22
+#define PIN_DPAD_DOWN   12
+#define PIN_DPAD_LEFT   13
+#define PIN_DPAD_RIGHT  11
+#define PIN_BUTTON_B1   2
+#define PIN_BUTTON_B2   3
+#define PIN_BUTTON_R2   4
+#define PIN_BUTTON_L2   5
+#define PIN_BUTTON_B3   6
+#define PIN_BUTTON_B4   7
+#define PIN_BUTTON_R1   8
+#define PIN_BUTTON_L1   9
+#define PIN_BUTTON_S1   16
+#define PIN_BUTTON_S2   17
+#define PIN_BUTTON_R3   14
+#define PIN_BUTTON_L3   15
+#define PIN_BUTTON_A1   20
+#define PIN_BUTTON_A2   21
+
+#define DEFAULT_SOCD_MODE SOCD_MODE_NEUTRAL
+#define BUTTON_LAYOUT BUTTON_LAYOUT_HITBOX
+
+
+#endif
